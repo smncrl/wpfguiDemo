402abbdc1426fb60e0c8b75123751b64787bd737
Simone Carella <simone.carella@ettsolutions.com>
2023-03-03 12:31:57 +0100

    *

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace wpfguiDemo;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    [wpfgui.Gui]
+    public float value = 0;
+
+    [wpfgui.Gui(0, 100)]
+    public float x = 0;
+
+    [wpfgui.Gui]
+    public string name = "Hello";
+
+    [wpfgui.Gui]
+    public Animals animal;
+
+    public enum Animals
+    {
+        Cat,
+        Dog,
+        Bird
+    }
+
+    [wpfgui.Gui]
+    public void Log()
+    {
+        Trace.WriteLine("Hello");
+    }
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        wpfgui.GUIWindow.Register(this);
+    }
+}
